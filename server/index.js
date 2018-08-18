eaa7ba625f5f2d609a8da2dc31104fa12753b4ce
jingshongshao <jinghongshao@yunify.com>
2018-08-18 11:54:29 +0800

    feature: 初始化项目目录

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const Koa = require('koa')
+const logger = require('koa-logger')
+const session = require('koa-session')
+const app = new Koa()
+
+app.keys = ['jinghong']
+
+app.use(logger())
+app.use(session(app))
+
+app.use(ctx => {
+  if (ctx.path === '/favicon.ico') return 
+
+  let n = ctx.session.views || 0
+  ctx.session.views = ++ n
+  ctx.body = n + ' views'
+
+})
+
+app.listen(2333)
