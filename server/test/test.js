e57eadb96030fd39b81fbaa1370961c1cdce818d
jingshongshao <jinghongshao@yunify.com>
2018-08-21 21:57:58 +0800

    demo for async function

--- server/test/test.js
+++ server/test/test.js
@@ -1,18 +1,72 @@
+const util = require('util')
+const { readFile } = require('fs')
+const path = require('path')
 
-// const puppeteer = require('puppeteer')
-// const path = require('path')
+// readFileAsync = util.promisify(readFile)
+
+// promise then的写法
+// readFileAsync(path.join(__dirname, 'test.txt'), 'utf8').then((data) => {
+//   console.log(`文件内容为:${data}` )
+// }).catch((error) => {
+//   console.log(error)
+// })
+
+// 或者使用sync函数 , 一个自执行的异步函数
+// ;(async () => {
+//    const data = await readFileAsync(path.join(__dirname, 'test.txt'), 'utf8')
+//    const data_2 = await readFileAsync(path.join(__dirname, 'test_2.txt'), 'utf8')
+
+//   console.log(data, data_2)
+// })()
+
+
+// ;(function * (){
+//   const data1 = yield readFileAsync(path.join(__dirname, 'test.txt'), 'utf8')
+//   const data2 = yield readFileAsync(path.join(__dirname, 'test_2.txt'), 'utf8')
+
+//   console.log(data1)
+//   console.log(data2)
+//   console.log('faf');
+
+// })()
+
+
+// 自己包装promise
+
+const testPath = path.join(__dirname, 'test.txt')
+
+const readFileAsync = (path) => {
+  return new Promise((resolve, reject) => {
+    readFile(path, 'utf8', (data, err) => {
+      if (data) resolve(data)
+      else reject(err)
+    })
+  })
+}
+
+// readFileAsync(testPath).then((data) => {
+//   console.log(data)
+// }, (err) => {
+//   console.log(err)
+// })
 
 // ;(async () => {
-//   const browser = await puppeteer.launch({
-//     args: ['--no-sandbox'],
-//     dumpio: false,
-//     executablePath: path.join(__dirname, '../../chromium/Chromium.app/Contents/MacOS/Chromium',),
-//     headless: false
-//   })
-//   const page = await browser.newPage();
-//   await page.goto('https://tuchong.com/1629931/20846352/#image353960409')
-//   await page.screenshot({path: 'example.png'});
-
-//   await browser.close()
+//   try {
+//     const data = await readFileAsync(testPath)
+//     console.log(data)
+//   } catch (err) {
+//     console.log(err)
+//   }
 // })()
+
+
+;(async () => {
+  await readFile(testPath, 'uff8')
+})().then((data) => {
+  console.log(data)
+}, (err) => {
+  console.log(err)
+})
+
+
 
