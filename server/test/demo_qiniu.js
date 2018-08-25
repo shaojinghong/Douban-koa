69792c4bb06c0b1c378faad9c4fb3e6e0dfc4db3
jingshongshao <jinghongshao@yunify.com>
2018-08-25 21:45:21 +0800

    通过mongoose操作mongoDB数据库,完成建模工作

--- server/test/demo_qiniu.js
+++ server/test/demo_qiniu.js
@@ -5,7 +5,7 @@ const secretKey = 'BJYJ1lkWOAJcpyAYwC_MF1FPJ9Lt4i6Xu7JfrdOE'
 // 鉴权对象mac
 const mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
 const config = new qiniu.conf.Config();
-config.zone = qiniu.zone.Zone_z2
+config.zone = qiniu.zone.Zone_z1
 
 // 资源管理的操作对象
 const bucketManager = new qiniu.rs.BucketManager(mac, config);
@@ -14,19 +14,20 @@ const url = 'https://img3.doubanio.com/view/photo/l_ratio_poster/public/p2530772
 const bucket = 'images'
 const key = 'jinghong.png'
 
-// bucketManager.fetch(url, bucket, key, (err, respBody, respInfo) => {
-//   if (err) {
-//     console.log(`发生异常${err}`)
-//   } else {
-//     if (respInfo.statusCode === 200) {
-//       console.log(`key: ${respBody.key} Hash: ${respBody.hash} 
-//         fsize: ${respBody.fsize} mimeType: ${respBody.mimeType}`)
-//     } else {
-//       console.log('错误status', respInfo.statusCode)
-//       console.log(respBody);
-//     }
-//   }
-// })
+// 回调方式
+bucketManager.fetch(url, bucket, key, (err, respBody, respInfo) => {
+  if (err) {
+    console.log(`发生异常${err}`)
+  } else {
+    if (respInfo.statusCode === 200) {
+      console.log(`key: ${respBody.key} Hash: ${respBody.hash} 
+        fsize: ${respBody.fsize} mimeType: ${respBody.mimeType}`)
+    } else {
+      console.log('错误status', respInfo.statusCode)
+      console.log(respBody);
+    }
+  }
+})
 
 // 封装成promise
 const uploadQiniu = (url, bucket, key) => {
@@ -46,10 +47,22 @@ const uploadQiniu = (url, bucket, key) => {
   })
 }
 
+then/catch方法调用
 uploadQiniu(url, bucket, key).then((resKey) => {
   console.log(resKey)
 }).catch((err) => {
   console.log(`初始化${err}`)
 })
+
+// async/await方法调用
+;(async () => {
+  console.log('start upload')
+  try {
+    const newKey = await uploadQiniu(url, bucket, key)
+    console.log(`上传结束，更新的key为${newKey}`)
+  } catch (err) {
+    console.log(err)
+  }
+})()
 
 
