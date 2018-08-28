3b14e55d4aa505e6ed00595db4d38ce8145e6cdd
jingshongshao <jinghongshao@yunify.com>
2018-08-28 23:34:02 +0800

    decorator demo

--- server/test/dec.js
+++ server/test/dec.js
@@ -1,88 +1,141 @@
+function decorateArmour(target, key, descriptor) {
 
-// const speak = (target, key, descriptor) => {
-//   console.log(target, key, descriptor)
-//   target.language = '中文'
-//   return descriptor
-// }
+  const method = descriptor.value;
+  let moreDef = 100;
+  let ret;
 
-// class Boy {
-//   @speak
-//   run () {
-//     console.log(`I can speak ${this.language}`)
-//   }
-// }
-
-// const jinghong = new Boy()
-
-// jinghong.run()
-
-// 调用实例
-@log 
-class Person{}
-// 实现代码
-const Person = log(Person);
-
-// decorator 处理
-function _applyDecoratedDescriptor(target, property, decorators, descriptor, context) {
-  var desc = {};
-  Object['ke' + 'ys'](descriptor).forEach(function (key) {
-    desc[key] = descriptor[key];
-  });
-  desc.enumerable = !!desc.enumerable;
-  desc.configurable = !!desc.configurable;
-
-  if ('value' in desc || desc.initializer) {
-    desc.writable = true;
-  }
-
-  desc = decorators.slice().reverse().reduce(function (desc, decorator) {
-    return decorator(target, property, desc) || desc;
-  }, desc);
-
-  if (context && desc.initializer !== void 0) {
-    desc.value = desc.initializer ? desc.initializer.call(context) : void 0;
-    desc.initializer = undefined;
-  }
-
-  if (desc.initializer === void 0) {
-    Object['define' + 'Property'](target, property, desc);
-    desc = null;
-  }
-
-  return desc;
-}
-
-// 调用实例
-class Person{
-  @log
-  say(){}
-}
-
-// 实现代码
-_applyDecoratedDescriptor(
-  Person.prototype, 
-  'say', 
-  [log],
-  Object.getOwnPropertyDescriptor(Person.prototype, 'say'),
-  Person.prototype)
-)
-
-@log
-class Person{
-  // 函数
-  @log
-  say(){}
+  console.log(method)
   
-  // 属性
-  @log
-  name = 'tec';
+  descriptor.value = (...args)=>{
+    args[0] += moreDef;
+    ret = method.apply(target, args);
+    return ret;
+  }
+
+  return descriptor;
 }
 
-// 同样适用于对象字面量的方法、属性
-const tec = {
+class Man{
+  constructor(def = 2,atk = 3,hp = 3){
+    this.init(def,atk,hp);
+  }
+
+  @decorateArmour
+  init(def,atk,hp){
+    this.def = def; // 防御值
+    this.atk = atk;  // 攻击力
+    this.hp = hp;  // 血量
+  }
+  toString(){
+    return `防御力:${this.def},攻击力:${this.atk},血量:${this.hp}`;
+  }
+}
+
+var tony = new Man();
+
+console.log(`当前状态 ===> ${tony}`);
+
+
+修饰类
+
+const init = (target) => {
+  target.nickName = 'person_name'
+  target.prototype.nickName = 'kitty'
+}
+
+@init
+class Person {
+  nickName = 'native_name'
+}
+
+const cat = new Person()
+
+console.log('Person', Person.nickName)  // 'Person person_name'
+console.log('instance', cat.nickName) // 'instance native_name'
+
+
+function readonly(target, name, descriptor){
+  // descriptor对象原来的值如下
+  // {
+  //   value: specifiedFunction,
+  //   enumerable: false,
+  //   configurable: true,
+  //   writable: true
+  // };
+  descriptor.writable = false;
+  return descriptor;
+}
+
+class Person {
+  @readonly
+  name() { 
+    return `${this.first} ${this.last}` 
+  }
+}
+
+const jack = new Person()
+
+console.log(jack.name())
+
+
+
+const log = (target, name, descriptor) => {
+
+  const oldAdd = descriptor.value
+
+  descriptor.value = function(){
+    console.log(`传入实例方法add的参数${arguments}`)
+    console.log(`实例中的age属性：${this.age}`)
+    return oldAdd.apply(this, arguments)
+  }
+
+  return descriptor
+}
+
+class Math {
   @log
-  name:'tec',
-  
+  add(a, b){
+    return a + b
+  }
+}
+
+var math = new Math()
+math.age = 10
+const value = math.add(3, 6)
+console.log(`value is equal to ${value}`)
+
+
+
+class Math {
   @log
-  walk(){}
-};
+  add(a, b) {
+    return a + b;
+  }
+}
+
+function log(target, name, descriptor) {
+  var oldValue = descriptor.value;
+
+  descriptor.value = function() {
+    console.log(`Calling ${name} with`, arguments);
+    // return oldValue.apply(this, arguments);
+  };
+
+  return descriptor;
+}
+
+const math = new Math();
+
+// passed parameters should get logged now
+console.log('结果为', math.add(2, 4));
+
+
+
+
+
+
+
+
+
+
+
