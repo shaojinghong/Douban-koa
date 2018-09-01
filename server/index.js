670d73f8ae188425bdd0454b286c4da9aee6568b
jingshongshao <jinghongshao@yunify.com>
2018-09-01 20:35:39 +0800

    封装路由，暴露数据API

--- server/index.js
+++ server/index.js
@@ -1,41 +1,43 @@
 const Koa = require('koa')
-const views = require('koa-views')
-const path = require('path')
-const mongoose = require('mongoose')
+const { resolve } = require('path')
 const { connect, initSchemas } = require('./database/init')
-const router = require('./routes/index')
-
-const app = new Koa()
+const mongoose = require('mongoose')
+const R = require('ramda')
+const MIDDLEWARES = ['router']
 
 ;(async() => {
+
+  const useMiddlewares = (app) => {
+    R.map(
+      R.compose(
+        R.forEachObjIndexed(
+          initWith => initWith(app)
+        ),
+        require,
+        name => resolve(__dirname, `./middlewares/${name}`)
+      )
+    )(MIDDLEWARES)
+  }
+
   try{
     // 链接数据库
     await connect()
     // 初始化Schema
     initSchemas()
-    // 爬取数据，并保存到数据库
-    // require('./tasks/movies')
-    // require('./tasks/api')    
+
   } catch (err) {
     console.log(err)
   }
+  
+  const app = new Koa()
+  // 加载中间件
+  await useMiddlewares(app)
+  console.log('service is start')
+  app.listen(2333)
+
 })()
 
-// koa-router的固定写法
-app
-  .use(router.routes())
-  .use(router.allowedMethods())
-
-app.use(views(path.join(__dirname, './views'), {
-  extension: 'pug'
-}))
-
-app.use(async (ctx, next) => {
-  await ctx.render('index', {
-    you: '世界',
-    me: 'jinghong'
-  })
-})
+    
 
 
-app.listen(2333)
+
--- start.js
+++ start.js
@@ -1,4 +1,4 @@
 require('babel-core')
 require("babel-register")
 require('babel-polyfill')
-require('./server/test/dec')
+require('./server/index.js')
