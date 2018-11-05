2eb6116478b95c224d1918cc6c761b4fa811fdc9
jingshongshao <jinghongshao@yunify.com>
2018-11-05 21:55:08 +0800

    Add proxy

--- src/lib/request.js
+++ src/lib/request.js
@@ -6,17 +6,19 @@ const defaultAxiosConf = {
 }
 
 const _request = (params = {}, fn = () => {}) => {
-  return axios({ ... defaultAxiosConf, ... params })
-    then(res => {
+  return axios({ ...defaultAxiosConf, ...params })
+    .then(res => {
       const { success, data, err, code } = res.data
 
       if (code === 401) {
-        window.localStorage.href = '/'
+        window.location.href = '/'
+
         return
       }
-      
+
       if (success) {
         fn(false)
+
         return data
       }
 
@@ -25,20 +27,20 @@ const _request = (params = {}, fn = () => {}) => {
     .catch(err => {
       fn(false)
 
-      message.err(String(err || 'New work is error'))
+      message.err(String(err || '网络错误'))
     })
-
 }
 
 export default (param) => {
   const type = typeof param
 
-  if (type === 'function'){
+  if (type === 'function') {
     param(true)
+
     return (obj) => _request(obj, param)
   }
 
-  if (type === 'object' && type != null) {
+  if (type === 'object' && type !== null) {
     return _request(param)
   }
 }
